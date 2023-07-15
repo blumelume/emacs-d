71f586db0079b339abc1dae1854e4124007993b9
Maximilian Rudolph <rudolph-max@gmx.net>
2023-07-15 12:12:33 +0200

    fix: proper web-mode config

--- custom/setup-prog-mode.el
+++ custom/setup-prog-mode.el
@@ -17,6 +17,15 @@
 
 (use-package emmet-mode)
 (use-package web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(setq web-mode-enable-block-face  nil)
+
 (use-package js2-mode)
 (use-package yasnippet)
 
